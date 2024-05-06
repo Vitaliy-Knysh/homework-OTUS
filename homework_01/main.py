c28eaddf6ea8ba7a87d4b2c39d9726a1d0059ae2
vitaliy <mr.knyshjr2@gmail.com>
2024-05-06 03:33:09 +0300

    выполнено ДЗ 1

---
+++ homework_01/main.py
@@ -0,0 +1,49 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers(numbers):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 5, 7)
+    <<< [1, 4, 25, 49]
+    """
+    return [*map(lambda x: x ** 2, numbers)]
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"  # простое число
+
+
+def is_prime(number):
+    if number % 1 == 0 and number > 1:
+        for i in range(2, number - 1):
+            if number % i == 0:
+                return False
+        return True
+    else: return False
+
+
+def filter_numbers(numbers, fltr):
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
+    match fltr:
+        case "odd":
+            return [*filter(lambda x: x % 2 == 1, numbers)]
+        case "even":
+            return [*filter(lambda x: x % 2 == 0, numbers)]
+        case "prime":
+            return [*filter(is_prime, numbers)]
